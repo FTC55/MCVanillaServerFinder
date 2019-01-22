9ef802a3261835f67f914a03d944a68439f5d31a
FTC55 <francescoiera@live.it>
2019-01-22 19:36:09 +0100

    Add files via upload
    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+<h1>MCVanillaServerFinder</h1>
+This tool lets you easily scan for 100% vanilla Minecraft Servers starting from one or more IPs of your choice. Usage is listed below.
+<h2>Usage</h2>
+<p>usage: serverfinder.py [-h] [--pnum PNUM] [--depth DEPTH] [--minp MINP[--maxp MAXP]
+                       startip mcversion</p>
+
+<p>positional arguments:</p>
+<li><strong>startip</strong>        The IP(s) to begin the search from. Interval with a comma and
+                 no spaces.
+  <li><strong>mcversion</strong>      The version(s) to search for. Interval with a comma and no
+                 spaces.
+<br />
+<p>optional arguments:</p>
+<li><strong>--pnum PNUM</strong>    Number of processes</li>
+  <li><strong>--depth DEPTH</strong>  Depth of IP scan (between 1 and 2)</li>
+  <li><strong>--minp MINP</strong>    Minimum amount of players atm</li>
+  <li><strong>--maxp MAXP</strong>    Maximum amount of players atm</li>
+  <h2>Warnings</H2>
+  <li>Note that a level 2 scan corresponds to a full /16 subnet mask so expect much longer wait times.</li>
+  <li>I am by no means an expert Python programmer, so the code is definitely untidy and not optimized, it's just a script that I 've quickly put together because I thought it would be useful. I'm also just getting started with GitHub so bear with me for any mistakes.</li>
+  <h2>Requirements</h2>
+  <li><strong>mcstatus</strong> by Dinnerbone (https://github.com/Dinnerbone/mcstatus)
+  <li><strong>argparse</strong>
---
+++ serverfinder.py
@@ -0,0 +1,58 @@
+import argparse
+from mcstatus import MinecraftServer
+from multiprocessing import Pool
+import time
+import socket
+parser = argparse.ArgumentParser()
+parser.add_argument('startip', type=str, help='The IP(s) to begin the search from. Interval with a comma and no spaces.')
+parser.add_argument('--pnum', type=int, help='Number of processes', default=None)
+parser.add_argument('--depth', type=int, help='Depth of IP scan', default="1")
+parser.add_argument('mcversion', type=str, help='The version(s) to search for. Interval with a comma and no spaces.')
+parser.add_argument('--minp', type=int, help='Minimum amount of players atm', default="0")
+parser.add_argument('--maxp', type=int, help='Maximum amount of players atm', default="1000")
+args = parser.parse_args()
+if args.depth > 2 or 1 > args.depth:
+    raise argparse.ArgumentTypeError('Depth has to be between 1 and 2')
+mcversion = args.mcversion.split(",")
+def listgenerator(ip, level):
+    iplist = []
+    ips = ip.split(",")
+    for ip in ips:
+        ip = socket.gethostbyname(ip)
+        split = ip.split(".")
+        if level == 1:
+            for i in range(255):
+                iplist.append(".".join(split[:3])+"."+str(i))
+        elif level == 2:
+            for i in range(255):
+                iplist.append(".".join(split[:2])+"."+str(i)+"."+split[3])
+            length = iplist
+            for x in length:
+                for i in range(255):
+                    split = x.split(".")
+                    iplist.append(".".join(split[:3])+"."+str(i))
+    return iplist
+def serverfinder(x):
+    server = MinecraftServer.lookup(x)
+    try:
+        query = server.query()
+        status = server.status()
+    except:
+        return
+    if query.software.version in mcversion and query.software.brand == "vanilla" and args.maxp >= query.players.online >= args.minp and "modinfo" not in status.raw:
+        print ("Hit!")
+        return x
+    
+if __name__ == '__main__':
+    p = Pool(args.pnum)
+    iplist = listgenerator(args.startip, args.depth)
+    t1 = time.time()
+    print ("Performing scan on IP {} with depth {} on {} processes.".format(args.startip, args.depth, args.pnum))
+    results = p.map(serverfinder, iplist)
+    p.close()
+    p.terminate()
+    results = list(filter(None, results))
+    with open('results.txt', 'w') as f:
+        for item in results:
+            f.write("%s\n" % item)
+    print("Job done!, took {}".format(time.time()-t1))
